fc2fda2036c4b681c9685006f52f44795ef85f29
James Dawson <james.dawson@endjin.com>
2022-07-08 13:45:50 +0100

    Add comment for placeholder tests script

--- tests/integration.tests.ps1
+++ tests/integration.tests.ps1
@@ -1,3 +1,6 @@
+# Placeholder file for integrating Pester tests with the build process
+
+# TODO: Run the Bicep module tests as part of the build process - although this will be slow
 Describe "Bicep Module Integration Tests" {
     Context "Dummy Module" {
         It "should be true" {
