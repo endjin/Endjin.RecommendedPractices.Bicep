6d9edf35e22fd2c29093df908588d87446914531
James Dawson <james.dawson@endjin.com>
2022-07-08 13:12:11 +0100

    Bump build module version

--- build.ps1
+++ build.ps1
@@ -39,7 +39,7 @@ param (
     [string] $BuildModulePath,
 
     [Parameter()]
-    [version] $BuildModuleVersion = "0.2.1"
+    [version] $BuildModuleVersion = "0.2.2"
 )
 
 $ErrorActionPreference = $ErrorActionPreference ? $ErrorActionPreference : 'Stop'
