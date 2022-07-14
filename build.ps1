645c3dad56300393b3329e4ec9cd8648ac81633e
James Dawson <james.dawson@endjin.com>
2022-07-14 16:31:01 +0100

    Add .gitattributes files for module README files (#19)
    * Ensure module README files are 'LF' line endings
    
    This should reduce noise from running the 'brm' tool
    
    * Bump build module version

--- build.ps1
+++ build.ps1
@@ -39,7 +39,7 @@ param (
     [string] $BuildModulePath,
 
     [Parameter()]
-    [version] $BuildModuleVersion = "0.2.2"
+    [version] $BuildModuleVersion = "0.2.3"
 )
 
 $ErrorActionPreference = $ErrorActionPreference ? $ErrorActionPreference : 'Stop'
