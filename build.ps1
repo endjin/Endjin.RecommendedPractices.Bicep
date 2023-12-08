67aa3e53186922ae4c4057437d9cbea1090b4595
dependjinbot <dependjinbot@endjin.com>
2023-12-08 02:23:07 +0000

    Updated scripted build to 1.5.3

--- build.ps1
+++ build.ps1
@@ -39,7 +39,7 @@ param (
     [string] $BuildModulePath,
 
     [Parameter()]
-    [version] $BuildModuleVersion = "1.5.2"
+    [version] $BuildModuleVersion = "1.5.3"
 )
 
 $ErrorActionPreference = $ErrorActionPreference ? $ErrorActionPreference : 'Stop'
