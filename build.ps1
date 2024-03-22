3019dc81287a712ebdb8c207227ed7c19a2fd3fa
dependjinbot <dependjinbot@endjin.com>
2024-03-22 02:16:48 +0000

    Updated one or more module dependencies in build.ps1

--- build.ps1
+++ build.ps1
@@ -39,7 +39,7 @@ param (
     [string] $BuildModulePath,
 
     [Parameter()]
-    [version] $BuildModuleVersion = "1.5.4"
+    [version] $BuildModuleVersion = "1.5.5"
 )
 
 $ErrorActionPreference = $ErrorActionPreference ? $ErrorActionPreference : 'Stop'
